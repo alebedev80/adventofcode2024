be73d87bc476c9424d2c06fbbbe9260e9ecde4d7
Andrey Lebedev <andrew.lebedev@gmail.com>
2024-12-08 23:23:40 +0400

    day2 task implementation

--- day02_1/main.go
+++ day02_1/main.go
@@ -1,120 +1,112 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
-	"sync/atomic"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
+    "sync"
+    "sync/atomic"
 )
 
 type LevelType int
 
 const (
-	LevelInvalid LevelType = iota
-	LevelIncreasing
-	LevelDecreasing
+    LevelInvalid LevelType = iota
+    LevelIncreasing
+    LevelDecreasing
 )
 
 type level []int16
 
 func (l level) getLevelType() LevelType {
-	var increasing, decreasing int
-	var incCalcStop, decCalcStop bool
-	llen := len(l)
-	for i := 1; i < llen; i++ {
-		diff := l[i] - l[i-1]
-		if !incCalcStop && diff > 0 && diff < 4 {
-			increasing++
-		} else if !decCalcStop && diff < 0 && diff > -4 {
-			decreasing++
-		}
-		if increasing != i {
-			incCalcStop = true
-		}
-		if decreasing != i {
-			decCalcStop = true
-		}
+    var increasing, decreasing int
+    var incCalcStop, decCalcStop bool
+    for i := 1; i < l.len(); i++ {
+        diff := l[i] - l[i-1]
+        if !incCalcStop && diff > 0 && diff < 4 {
+            increasing++
+        } else if !decCalcStop && diff < 0 && diff > -4 {
+            decreasing++
+        }
 
-		if incCalcStop && decCalcStop {
-			return LevelInvalid
-		}
-	}
-	if !incCalcStop {
-		return LevelIncreasing
-	}
-	return LevelDecreasing
+        incCalcStop = increasing != i
+        decCalcStop = decreasing != i
+
+        if incCalcStop && decCalcStop {
+            return LevelInvalid
+        }
+    }
+    if !incCalcStop {
+        return LevelIncreasing
+    }
+    return LevelDecreasing
 }
 
 func (l level) len() int {
-	return len(l)
+    return len(l)
 }
 
 func (l level) levelWithOutElement(i int) level {
-	var newLevel level
-	for j, v := range l {
-		if j != i {
-			newLevel = append(newLevel, v)
-		}
-	}
-	return newLevel
+    var newLevel level
+    for j, v := range l {
+        if j != i {
+            newLevel = append(newLevel, v)
+        }
+    }
+    return newLevel
 }
 
 func main() {
 
-	path := os.Args[1]
-	scanner, closef := fileReader(path)
-	defer closef()
-	var wg sync.WaitGroup
-	var result uint32
+    path := os.Args[1]
+    scanner, closef := fileReader(path)
+    defer closef()
+    var wg sync.WaitGroup
+    var result uint32
 
-	for scanner.Scan() {
-		wg.Add(1)
-		go func(line string) {
-			defer wg.Done()
-			l := checkLevel(line)
-			if l == LevelIncreasing || l == LevelDecreasing {
-				atomic.AddUint32(&result, 1)
-			}
-		}(scanner.Text())
-	}
-	wg.Wait()
+    for scanner.Scan() {
+        wg.Add(1)
+        go func(line string) {
+            defer wg.Done()
+            if checkLevel(line) != LevelInvalid {
+                atomic.AddUint32(&result, 1)
+            }
+        }(scanner.Text())
+    }
+    wg.Wait()
 
-	fmt.Printf("%d reports are safe\n", result)
+    fmt.Printf("%d reports are safe\n", result)
 }
 
 func checkLevel(s string) LevelType {
-	stringList := strings.Fields(s)
+    stringList := strings.Fields(s)
 
-	l := level{}
-	for _, s := range stringList {
-		num, err := strconv.Atoi(s)
-		if err != nil {
-			panic(fmt.Sprintf("Error converting string to integer: %v", err))
-		}
-		l = append(l, int16(num))
-	}
+    l := level{}
+    for _, s := range stringList {
+        num, err := strconv.Atoi(s)
+        if err != nil {
+            panic(fmt.Sprintf("Error converting string to integer: %v", err))
+        }
+        l = append(l, int16(num))
+    }
 
-	llen := l.len()
-	i := 0
-	result := l.getLevelType()
-	for i = 0; result == LevelInvalid && i < llen; i++ {
-		l2 := l.levelWithOutElement(i)
-		result = l2.getLevelType()
-	}
-	return result
+    result := LevelInvalid
+    for i := 0; result == LevelInvalid && i < l.len(); i++ {
+        result = l.levelWithOutElement(i).getLevelType()
+    }
+    return result
 }
 
 func fileReader(path string) (*bufio.Scanner, func()) {
-	file, err := os.Open(path)
+    file, err := os.Open(path)
 
-	if err != nil {
-		panic(fmt.Sprintf("error opening file: %v", err))
-	}
+    if err != nil {
+        panic(fmt.Sprintf("error opening file: %v", err))
+    }
 
-	f := func() { file.Close() }
+    f := func() { file.Close() }
 
-	return bufio.NewScanner(file), f
+    return bufio.NewScanner(file), f
 }
